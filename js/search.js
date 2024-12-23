b1ab31a0eb87c4d37b0a98e12a65d17d4475bef0
AlexPlane555 <alexey.gass555@mail.ru>
2024-12-23 12:41:19 +0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -5,24 +5,23 @@ const reverseButton = document.getElementById("reverse-button");
 let words = [];
 let translations = [];
 
-// Замените 'words.txt' на путь к вашему файлу
 fetch('./js/words.txt')
   .then(response => response.text())
   .then(data => {
-    const lines = data.split('\n'); // Разделяем на строки
+    const lines = data.split('\n');
     lines.forEach(line => {
-      if (line.trim() !== "") { // Пропускаем пустые строки
-        const parts = line.split(' - '); // Разделяем строку по " - "
+      if (line.trim() !== "") {
+        const parts = line.split(' - ');
         if (parts.length === 2) {
           words.push(parts[0].trim());
-          translations.push(parts[1].trim());
+          translations.push(parts[1].trim().split(/\s+/).map(s => s.trim())); // Разбиваем перевод по пробелам
         } else {
           console.error("Ошибка в строке:", line);
         }
       }
     });
-    words.sort(); // Сортируем слова после загрузки
-    displayWords(); // Отображаем слова после загрузки и сортировки
+    words.sort();
+    displayWords();
     searchInput.addEventListener("input", searchWords);
     reverseButton.addEventListener("click", reverseTranslation);
   })
@@ -33,7 +32,7 @@ fetch('./js/words.txt')
 
 function displayWords() {
   resultsList.innerHTML = "";
-  words.forEach((word, index) => addWord(word, translations[index]));
+  words.forEach((word, index) => addWord(word, translations[index].join(', ')));
 }
 
 function searchWords() {
@@ -44,7 +43,7 @@ function searchWords() {
   } else {
     words.forEach((word, index) => {
       if (word.toLowerCase().includes(searchTerm)) {
-        addWord(word, translations[index]);
+        addWord(word, translations[index].join(', '));
       }
     });
   }
@@ -56,8 +55,8 @@ function reverseTranslation() {
   if (searchTerm === "") return;
 
   translations.forEach((translation, index) => {
-    if (translation.toLowerCase().includes(searchTerm)) {
-      addWord(translation, words[index]);
+    if (translation.join(' ').toLowerCase().includes(searchTerm)) {
+      addWord(translation.join(', '), words[index]);
     }
   });
 }
